092b6f3c5185dc02d4f52332add22688828d9328
Cristopher Cruz <ccruz-2026541@kinal.edu.gt>
2026-07-14 17:25:41 -0600

    Creamos la ventana persoanalizada y agregamos formulario

---
+++ javafx-simulacion-login-ves/src/com/cristophercruz/model/Usuario.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cristophercruz.model;
+
+/**
+ *
+ * @author informatica
+ */
+public class Usuario {
+    private String nombreUsuario;
+    private String password;
+    private String nombreCompleto;
+    private Rol role;
+    
+    public Usuario() {
+        
+    }
+    
+    public Usuario(String nombreUsuario, String password, String nombreCompleto, Rol role ){
+        
+        
+    }
+    
+    
+}
